ce287160519059bf75dfb510ebbba8342f1f3ce1
Andrew Halle <ahalle@berkeley.edu>
2018-10-31 14:43:39 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import random
+import requests
+import webbrowser
+
+base_url = "https://doc.python.org/3/library/"
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "html.parser")
+links = [x.get('href') for x in soup.find_all('ul')[1].find_all('a')]
+link = random.choice(links)
+webbrowser.open(base_url + link)
